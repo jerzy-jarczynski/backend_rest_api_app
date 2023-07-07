ef84ef9b4eabece9a62ac19818781eb6f375402b
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-07-07 22:13:41 +0200

    Add separate db file, add concerts && seats, import db to server

---
+++ db.js
@@ -0,0 +1,29 @@
+const db = {
+  testimonials: [
+    { id: '1', author: 'John Doe', text: 'This company is worth every coin!' },
+    { id: '2', author: 'Amanda Doe', text: 'They really know how to make you happy.' },
+    { id: '3', author: 'Jack Black', text: 'You must never underestimate the power of the eyebrow.' },
+    { id: '4', author: 'Dave Grohl', text: 'I had no idea what I was doing and I faked it and it worked.' },
+    { id: '5', author: 'David Gilmour', text: 'Everything in moderation - that\'s what I live by.' },
+    { id: '6', author: 'John Lennon', text: 'I\'ve always been politically minded and against the status quo.' },
+    { id: '7', author: 'Paul McCartney', text: 'Think globally, act locally.' },
+    { id: '8', author: 'Jack White', text: 'I consider music to be storytelling, melody and rhythm.' },
+    { id: '9', author: 'Ed Sheeran', text: 'I\'ve fallen for your eyes, but they don\'t know me yet.' },
+    { id: '10', author: 'Freddy Mercury', text: 'I won\'t be a rock star.' },
+  ],
+  concerts: [
+    { id: 1, performer: 'John Doe', genre: 'Rock', price: 25, day: 1, image: '/img/uploads/1fsd324fsdg.jpg' },
+    { id: 2, performer: 'Rebekah Parker', genre: 'R&B', price: 25, day: 1, image: '/img/uploads/2f342s4fsdg.jpg' },
+    { id: 3, performer: 'Maybell Haley', genre: 'Pop', price: 40, day: 1, image: '/img/uploads/hdfh42sd213.jpg' },
+  ],
+  seats: [
+    { id: 1, day: 1, seat: 3, client: 'Amanda Doe', email: 'amandadoe@example.com' },
+    { id: 2, day: 1, seat: 9, client: 'Curtis Johnson', email: 'curtisj@example.com'  },
+    { id: 3, day: 1, seat: 10, client: 'Felix McManara', email: 'felxim98@example.com'  },
+    { id: 4, day: 1, seat: 26, client: 'Fauna Keithrins', email: 'mefauna312@example.com'  },
+    { id: 5, day: 2, seat: 1, client: 'Felix McManara', email: 'felxim98@example.com'  },
+    { id: 6, day: 2, seat: 2, client: 'Molier Lo Celso', email: 'moiler.lo.celso@example.com'  },
+  ],
+}
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,42 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
+const db = require('./db');
 
 const app = express();
-
-const db = [
-  { id: '1', author: 'John Doe', text: 'This company is worth every coin!' },
-  { id: '2', author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-  { id: '3', author: 'Jack Black', text: 'You must never underestimate the power of the eyebrow.' },
-  { id: '4', author: 'Dave Grohl', text: 'I had no idea what I was doing and I faked it and it worked.' },
-  { id: '5', author: 'David Gilmour', text: 'Everything in moderation - that\'s what I live by.' },
-  { id: '6', author: 'John Lennon', text: 'I\'ve always been politically minded and against the status quo.' },
-  { id: '7', author: 'Paul McCartney', text: 'Think globally, act locally.' },
-  { id: '8', author: 'Jack White', text: 'I consider music to be storytelling, melody and rhythm.' },
-  { id: '9', author: 'Ed Sheeran', text: 'I\'ve fallen for your eyes, but they don\'t know me yet.' },
-  { id: '10', author: 'Freddy Mercury', text: 'I won\'t be a rock star.' },
-];
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-// get all db array
+// get all db.testimonials array
 app.get('/testimonials', (req, res) => {
-  res.json(db);
+  res.json(db.testimonials);
 });
 
-// get random db array element
+// get random db.testimonials array element
 app.get('/testimonials/random', (req, res) => {
-  const randomIndex = Math.floor(Math.random() * db.length);
-  const randomTestimonial = db[randomIndex];
+  const randomIndex = Math.floor(Math.random() * db.testimonials.length);
+  const randomTestimonial = db.testimonials[randomIndex];
   res.json(randomTestimonial);
 });
 
-// get single db array element
+// get single db.testimonials array element
 app.get('/testimonials/:id', (req, res) => {
   const id = parseInt(req.params.id);
-  const testimonial = db.find((item) => item.id === id);
+  const testimonial = db.testimonials.find((item) => item.id === id);
   if (testimonial) {
     res.json(testimonial);
   } else {
@@ -44,7 +32,7 @@ app.get('/testimonials/:id', (req, res) => {
   }
 });
 
-// post add new element to db array
+// post add new element to db.testimonials array
 app.post('/testimonials', (req, res) => {
   const { author, text } = req.body;
 
@@ -54,11 +42,11 @@ app.post('/testimonials', (req, res) => {
 
   const id = uuidv4();
   const newTestimonial = { id, author, text };
-  db.push(newTestimonial);
+  db.testimonials.push(newTestimonial);
   res.status(201).json({ message: 'OK' });
 });
 
-// put modify db array element
+// put modify db.testimonials array element
 app.put('/testimonials/:id', (req, res) => {
   const { id } = req.params;
   const { author, text } = req.body;
@@ -67,7 +55,7 @@ app.put('/testimonials/:id', (req, res) => {
     return res.status(400).json({ error: 'Author and text are required fields' });
   }
 
-  const testimonial = db.find((item) => item.id === id);
+  const testimonial = db.testimonials.find((item) => item.id === id);
 
   if (!testimonial) {
     return res.status(404).json({ message: 'Testimonial not found' });
@@ -79,18 +67,23 @@ app.put('/testimonials/:id', (req, res) => {
   res.json({ message: 'OK' });
 });
 
-// delete element from db array
+// delete element from db.testimonials array
 app.delete('/testimonials/:id', (req, res) => {
   const testimonialId = req.params.id;
 
-  const testimonialIndex = db.findIndex((testimonial) => testimonial.id === testimonialId);
+  const testimonialIndex = db.testimonials.findIndex((testimonial) => testimonial.id === testimonialId);
 
   if (testimonialIndex !== -1) {
-    db.splice(testimonialIndex, 1);
+    db.testimonials.splice(testimonialIndex, 1);
     res.json({ message: 'OK' });
   } else {
     res.status(404).json({ message: 'Testimonial not found' });
   }
+});
+
+// catching bad links
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found...' });
 });
 
 app.listen(8000, () => {
