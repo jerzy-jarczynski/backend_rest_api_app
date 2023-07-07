f90b697ca61fae29e1d366845bb04e2ec1312629
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-07-07 20:58:59 +0200

    Add get endpoints

--- server.js
+++ server.js
@@ -20,6 +20,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
+// get all db array
+app.get('/testimonials', (req, res) => {
+  res.json(db);
+});
+
+// get random db array element
+app.get('/testimonials/random', (req, res) => {
+  const randomIndex = Math.floor(Math.random() * db.length);
+  const randomTestimonial = db[randomIndex];
+  res.json(randomTestimonial);
+});
+
+// get single db array element
+app.get('/testimonials/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const testimonial = db.find((item) => item.id === id);
+  if (testimonial) {
+    res.json(testimonial);
+  } else {
+    res.status(404).json({ error: 'Testimonial not found' });
+  }
+});
+
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
