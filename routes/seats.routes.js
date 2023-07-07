db8ed13da491a5b94d69409b0efc5bbfd62d36a9
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-07-07 23:18:20 +0200

    Add seats routes. Add start script to package.json

---
+++ routes/seats.routes.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./../db');
+const { v4: uuidv4 } = require('uuid');
+
+// get all db.seats array
+router.route('/seats').get((req, res) => {
+  res.json(db.seats);
+});
+
+// get random db.seats array element
+router.route('/seats/random').get((req, res) => {
+  const randomIndex = Math.floor(Math.random() * db.seats.length);
+  const randomSeat = db.seats[randomIndex];
+  res.json(randomSeat);
+});
+
+// get single db.seats array element
+router.route('/seats/:id').get((req, res) => {
+  const id = req.params.id;
+  const seat = db.seats.find((item) => item.id === id);
+  if (seat) {
+    res.json(seat);
+  } else {
+    if (id !== 'random')
+      res.status(404).json({ error: 'Seat not found' });
+  }
+});
+
+// post add new element to db.seats array
+router.route('/seats').post((req, res) => {
+  const { day, seat, client, email } = req.body;
+
+  if (!day || !seat || !client || !email) {
+    return res.status(400).json({ error: 'One or more mandatory fields omitted.' });
+  }
+
+  const id = uuidv4();
+  const newSeat = { id, day, seat, client, email };
+  db.seats.push(newSeat);
+  res.status(201).json({ message: 'OK' });
+});
+
+// put modify db.seats array element
+router.route('/seats/:id').put((req, res) => {
+  const { id } = req.params;
+  const { day, seat, client, email } = req.body;
+
+  if (!day || !seat || !client || !email) {
+    return res.status(400).json({ error: 'One or more mandatory fields omitted.' });
+  }
+
+  const modSeat = db.seats.find((item) => item.id === id);
+
+  if (!modSeat) {
+    return res.status(404).json({ message: 'Seat not found' });
+  }
+
+  modSeat.day = day;
+  modSeat.seat = seat;
+  modSeat.client = client;
+  modSeat.email = email;
+
+  res.json({ message: 'OK' });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const app = express();
 // import routes
 const testimonialRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
+const seatsRoutes = require('./routes/seats.routes');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', testimonialRoutes); // add testimonial routes to server
 app.use('/api', concertsRoutes); // add concerts routes to server
+app.use('/api', seatsRoutes); // add seats routes to server
 
 // catching bad links
 app.use((req, res) => {
