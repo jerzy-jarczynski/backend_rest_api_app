f815388d0eea6646a6d01b6075ca9d160e6f5302
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-07-09 18:30:41 +0200

    Fix store.js, app is working

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,4 +1,4 @@
-import { legacy_createStore as createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 // import reducers
@@ -11,12 +11,10 @@ const rootReducer = combineReducers({
   seats,
 });
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
-  compose(
-		applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 export default store;
