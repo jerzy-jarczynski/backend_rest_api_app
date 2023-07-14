fdbc63801914edf79f8c14b5631afa22f4e0bf5c
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-07-14 23:12:18 +0200

    Add socket.io. Integrate with the server.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./db');
 const path = require('path');
+const socket = require('socket.io');
 
 const app = express();
 
@@ -30,6 +31,12 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
 
-app.listen(process.env.PORT || 8000, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
 });
