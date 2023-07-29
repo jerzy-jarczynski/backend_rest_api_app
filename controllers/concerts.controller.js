1ea437fb848d8e1bd717534a69426643e19ad155
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-07-29 21:47:09 +0200

    Add data to database, change concert model, change concert controller getAll

--- controllers/concerts.controller.js
+++ controllers/concerts.controller.js
@@ -2,7 +2,8 @@ const Concert = require('../models/concert.model');
 
 exports.getAll = async (req, res) => {
   try {
-    res.json(await Concert.find());
+    const concerts = await Concert.find().populate('workshops');
+    res.json(concerts);
   }
   catch(err) {
     res.status(500).json({ message: err });
--- models/concert.model.js
+++ models/concert.model.js
@@ -6,6 +6,7 @@ const concertSchema = new mongoose.Schema({
   price: { type: Number, required: true },
   day: { type: Number, required: true },
   image: { type: String, required: true },
+  workshops: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Workshop' }],
 });
 
 module.exports = mongoose.model('Concert', concertSchema);
